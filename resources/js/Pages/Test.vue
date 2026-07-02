ae4ed4163bdae6a34149fca19068d88aec8f3ce5
TiaanF <tiaanf@8ohm.co.za>
2026-07-02 23:33:22 +0200

    chore: remove unused Test.vue page component

--- resources/js/Pages/Test.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import component from '/home/tiaanf/Dev/ohmsite/resources/assets/components/application-ui/data-display/description-lists/left_aligned_with_inline_actions.vue';
-</script>
-
-<template>
-
-    <component />
-
-</template>
