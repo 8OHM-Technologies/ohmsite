1ed498c3c8c6820ba1cef4bde8b6a781e7dc3373
TiaanF <tiaanf@8ohm.co.za>
2026-07-11 11:25:49 +0200

    feat: add sitemap configuration and console command for XML generation

---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
+use Carbon\Carbon;
+
+class GenerateSitemap extends Command
+{
+    protected $signature = 'sitemap:generate';
+    protected $description = 'Generate the XML sitemap';
+
+    public function handle(): void
+    {
+        Sitemap::create()
+            ->add(Url::create('/')
+                ->setLastModificationDate(Carbon::yesterday())
+                ->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY)
+                ->setPriority(1.0)
+            )
+            ->add(Url::create('/demo')
+                ->setLastModificationDate(Carbon::yesterday())
+                ->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY)
+                ->setPriority(0.9)
+            )
+            ->writeToFile(public_path('sitemap.xml'));
+
+        $this->info('Sitemap generated successfully.');
+    }
+}
